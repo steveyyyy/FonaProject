af270624af4197d7ad14c90669a2b65300e4bb68
steveyyyy <3601935-steveyyyy@users.noreply.gitlab.com>
2020-11-10 10:55:59 +0100

    finish uart

---
+++ src/uart/uart.cpp
@@ -0,0 +1,113 @@
+#include "uart.h"
+
+UART::UART(){
+    uart_dev = NULL;
+    baudrate = 115200;
+    message="";    
+}
+
+void UART::initHW(){
+    //Get device
+    uart_dev = device_get_binding("UART_1");
+	if (!uart_dev) {
+		printk("Cannot get UART device\n");
+	}
+
+    //Config device
+    uart_cfg.baudrate = baudrate;
+    uart_cfg.parity = UART_CFG_PARITY_NONE;
+    uart_cfg.stop_bits = UART_CFG_STOP_BITS_1;
+    uart_cfg.data_bits = UART_CFG_DATA_BITS_8;
+    uart_cfg.flow_ctrl = UART_CFG_FLOW_CTRL_NONE;
+
+    int ret = uart_configure(uart_dev, &uart_cfg);
+	if (ret != 0) {
+		printk("Cannot configure UART device\n");
+	}
+    checkConfig();
+}
+
+void UART::setBaudrate(int baudrate)
+{
+    this->baudrate = baudrate;
+}
+
+void UART::checkConfig(){
+ 	if (uart_config_get(uart_dev, &uart_cfg_check) != 0) {
+		printk("Cannot read config of UART device\n");
+	}
+}
+
+bool UART::enableRXInterrupt(){
+    bool processed=false;
+    if(!uart_dev){
+        printk("Problem to load uart device\n");
+    }
+    else{
+        processed=true;
+        uart_irq_callback_set(uart_dev, UART::uartRecive);
+        uart_irq_rx_enable(uart_dev);
+        //printk("UART device loaded... [OK]");
+    }
+    return processed;
+}
+
+void UART::uartSend(const char* txData) 
+{
+    u8_t txSize = strlen(txData);
+    for (int i=0; i<txSize; i++)
+    {
+        uart_poll_out(uart_dev, txData[i]);
+    };
+}
+
+void UART::uartRecive(struct device* dev){
+    u8_t recvData = 0;
+    if (!uart_irq_update(dev))
+    {
+        printk("retval should always be 1");
+        return;
+    }
+    if (uart_irq_rx_ready(dev))
+    {
+        uart_fifo_read(dev,&recvData,1);
+        getInstance()->message += recvData;
+        if(recvData == '\n'){
+            getInstance()->notify();
+            printk("%c",recvData);
+        }
+    }
+}
+
+UART* UART::getInstance(){
+    return &uart;
+}
+
+void UART::subscribe(IUARTObserver* subscriber) 
+{
+    vector<IUARTObserver*>::iterator it;
+    it = find(subscribers.begin(), subscribers.end(), subscriber);
+    if (it == subscribers.end())
+    {
+        subscribers.push_back(subscriber);
+    }    
+}
+
+void UART::unsubscribe(IUARTObserver* subscriber) 
+{
+    vector<IUARTObserver*>::iterator it;
+    it = find(subscribers.begin(), subscribers.end(), subscriber);
+    if (it != subscribers.end())
+    {
+        subscribers.erase(it);
+    }   
+}
+
+void UART::notify() 
+{
+    vector<IUARTObserver*>::iterator it;
+    for (it = subscribers.begin(); it != subscribers.end(); ++it)
+    {
+        (*it)->onMesssage(message);
+    }
+}
--- src/uart/uart.h
+++ src/uart/uart.h
@@ -1,11 +1,46 @@
 #ifndef UART_ONCE
 #define UART_ONCE
 
-#include "../xf/xf.h"
+#include <drivers/uart.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
 
-class UART : public IReactive
+using namespace std;
+
+class UART
 {
     public:
+        class IUARTObserver
+        {
+            public:
+            virtual void onMesssage(string message);
+        };
+        UART();
+        ~UART();
+        static UART* getInstance();
+        void initHW();
+        void checkConfig();
+        void setBaudrate(int baudrate);
+
+        void uartSend(const char* txData);
+        static void uartRecive(struct device* dev);
+        bool enableRXInterrupt();
+
+        void subscribe(IUARTObserver* subscriber);
+        void unsubscribe(IUARTObserver* subscriber);
+        void notify();
+
+    protected:
+        vector<IUARTObserver*> subscribers;
+        struct device* uart_dev;  /**< device structur driver */ 
+        struct uart_config uart_cfg;    /**< UART configuration */
+        struct uart_config uart_cfg_check;  /**< UART configuration check */
+        int baudrate;
+        string message;
+        static UART uart;
+
     
 };
 
