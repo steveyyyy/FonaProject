24104a0c0ba99189de4102b2195e5202093ab519
steveyyyy <3601935-steveyyyy@users.noreply.gitlab.com>
2020-12-03 17:30:10 +0100

    add functions

--- src/hw/display/display.cpp
+++ src/hw/display/display.cpp
@@ -1,16 +1,32 @@
 #include "display.h"
 
-Display::Display(u8_t command=254, u8_t specialCommand=124){
-    //this->uart=uart;
+Display::Display(const char* deviceBinding,int baudrate, u8_t command=0xFE, u8_t specialCommand=0x7C):UART(deviceBinding,baudrate){
     this->command=command;
     this->specialCommand=specialCommand;
 }
 
 void Display::clearDispaly(){
-    //uart->uartSend((char *)command);
-    //uart->uartSend((char *)(u8_t)1);
+    uartSend((char *)command);
+    uartSend((char *)(u8_t)1);
 }
 
-void Display::changeBaudrate(int baudrate){
-
+void Display::updateBaudrate(int baudrate){
+    uartSend((char *)specialCommand);
+    //uartSend((char *)(u8_t)baudrate);
+}
+void Display::setBacklight(u8_t brightness){
+    uartSend((char *)specialCommand);
+    uartSend((char *)brightness);
+}
+void Display::setCursorPosition(u8_t position){
+    uartSend((char *)command);
+    position = 0x80+position;
+    if(position <= 0xCF){
+        uartSend((char *)position);
+    }  
+}
+void Display::changeSplashScreen(string splashScreen){
+    setCursorPosition(0x00);
+    uartSend(splashScreen.c_str());
+    uartSend((char *)specialCommand);
 }
--- src/hw/display/display.h
+++ src/hw/display/display.h
@@ -1,21 +1,23 @@
 #include "../../uart/uart.h"
 #include <vector>
+#include <string>
 
 #ifndef DISPLAY_ONCE
 #define DISPLAY_ONCE
 
-
-class Display
+class Display: public UART
 {
 public:
-    Display(u8_t command, u8_t specialCommand);
+    Display(const char* deviceBinding,int baudrate,u8_t command, u8_t specialCommand);
     ~Display();
     void initHW();
     void clearDispaly();
 
-    void changeBaudrate(int baudrateOptions);
+    void updateBaudrate(int baudrate);
+    void setBacklight(u8_t brightness);
+    void setCursorPosition(u8_t position);
+    void changeSplashScreen(string splashScreen);
 private:
-    // UART* uart;
     u8_t command;
     u8_t specialCommand;  
     int baudrateOptions[6]={2400, 
